64ff4ad8f0f7d122697c8b1de854159e31b46f90
dong <2389314853@qq.com>
2024-12-22 06:12:15 +0000

    Authentication lab - exercise (genres)

--- index.js
+++ index.js
@@ -4,21 +4,20 @@ import cors from 'cors';
 import usersRouter from './api/users/index.js';
 import './db/index.js';
 import defaultErrHandler from './caller/errHandler';
-import moviesRouter from './api/movies/index.js';   //import movies router
-import authenticate from './authenticate/index.js';
-
-
+import moviesRouter from './api/movies/index.js';
+import authenticate from './authenticate/index.js'; // 导入认证中间件
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT; 
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
 app.use(defaultErrHandler);
-app.use('/api/movies', moviesRouter); 
+
+// 将认证中间件和 moviesRouter 合并，只使用一次 '/api/movies'
 app.use('/api/movies', authenticate, moviesRouter);
 
 app.listen(port, () => {
